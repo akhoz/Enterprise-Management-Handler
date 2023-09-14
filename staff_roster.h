6b7cb6717b38fe916fee521f994f1056ade22bb0
akhoz <adrian25092005@gmail.com>
2023-09-13 19:19:04 -0600

    fixing the header

--- staff_roster.h
+++ staff_roster.h
@@ -5,44 +5,54 @@
 using std::string;
 
 //Staff struct, used to store the information of the employees and create the Staff Roster linked list
-struct Staff {
+class Staff {
+public:
 	string staff_id; //Unique identifier of the employee
 	string job_title; //Kind of employee
 	string salary_bonuses; //Such as experience or other bonuses
 	string job_performed; //What the employee does
 	string employment_sector; //Sector where the staff performs his job, for example, production line
-	Workday* workdays; //Workdays of the employee
+	//Workday* workdays; //Workdays of the employee
 	Staff* boss; //Boss of the employee, null if the employee is the boss
 	Staff* next; //Next employee in the list
+	Staff* prev; //Previous employee in the list
 
 	//Constructor
-	Staff(string staff_id, string job_title, string salary_bonuses, string job_performed, string employment_sector, Workday* workdays, string boss_id) {
+	Staff(string staff_id, string job_title, string salary_bonuses, string job_performed, string employment_sector, string boss_id) {
 		//This is the constructor of the Staff struct, it receives all the information of the employee and creates the object, returns nothing
 		this->staff_id = staff_id;
 		this->job_title = job_title;
 		this->salary_bonuses = salary_bonuses;
 		this->job_performed = job_performed;
 		this->employment_sector = employment_sector;
-		this->workdays = workdays;
+		this->boss = nullptr;
+		//this->workdays = workdays;
 		this->next = nullptr;
-
-		this->boss = nullptr; //Set the boss to null, if the employee actually has a boss, it will be changed later
-		if (!boss_id.empty()) { //If the boss_id is not empty, it means that the employee has a boss, so we need to find it
-			Staff* current = this; //Create a pointer to the current employee
-			while (current) { //While the current employee is not null
-				if (current->staff_id == boss_id) { //If the current employee is the boss, set the boss pointer to the current employee
-					this->boss = current;
-					break;
-				}
-				current = current->next; //If the current employee is not the boss, move to the next employee
-			}
-		}
-
+		this->prev = nullptr;
 	}	
+
+	//Overload of the << operator, used to print the information of the employee
+	friend std::ostream& operator<<(std::ostream& os, const Staff& staff) {
+		os << "Staff ID: " << staff.staff_id << std::endl;
+		os << "Job Title: " << staff.job_title << std::endl;
+		os << "Salary Bonuses: " << staff.salary_bonuses << std::endl;
+		os << "Job Performed: " << staff.job_performed << std::endl;
+		os << "Employment Sector: " << staff.employment_sector << std::endl;
+		os << "Boss: ";
+		if (staff.boss) {
+			os << staff.boss->staff_id;
+		}
+		else {
+			os << "None";
+		}
+		os << "\n";
+		return os;
+	}
 };
 
 //Workday struct, used to store the information of the workdays of the employees
-struct Workday {
+class Workday {
+public:
 	string day; //Day of the week
 	string start_time; //Start time of the workday
 	string end_time; //End time of the workday
