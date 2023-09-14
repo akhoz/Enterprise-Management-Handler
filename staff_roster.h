9934ab646210f0b860e8167d1a6671b11129fa47
akhoz <adrian25092005@gmail.com>
2023-09-13 23:39:34 -0600

    changes

--- staff_roster.h
+++ staff_roster.h
@@ -17,8 +17,20 @@ public:
 	Staff* next; //Next employee in the list
 	Staff* prev; //Previous employee in the list
 
-	//Constructor
-	Staff(string staff_id, string job_title, string salary_bonuses, string job_performed, string employment_sector, string boss_id) {
+	Staff() {
+		//This is an empty constructor of the Staff struct, it creates an empty object, returns nothing
+		this->staff_id = "";
+		this->job_title = "";
+		this->salary_bonuses = "";
+		this->job_performed = "";
+		this->employment_sector = "";
+		this->boss = nullptr;
+		//this->workdays = nullptr;
+		this->next = nullptr;
+		this->prev = nullptr;
+	}
+
+	Staff(string staff_id, string job_title, string salary_bonuses, string job_performed, string employment_sector) {
 		//This is the constructor of the Staff struct, it receives all the information of the employee and creates the object, returns nothing
 		this->staff_id = staff_id;
 		this->job_title = job_title;
